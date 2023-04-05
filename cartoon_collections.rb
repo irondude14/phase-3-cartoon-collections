a864460d8ac430b55ed53e41590799ca0425d2c1
Vladyslav <vkadyrov@protonmail.com>
2023-04-04 20:20:27 -0400

    commit tree 84f7efe3bba29c10bdf76e0bad72b17262311de7

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) { |name, index| puts "#{index}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |name| "#{name.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? { |name| name.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = %w[cheddar gouda camembert]
+  cheese_types.find { |cheese| array.include?(cheese) }
 end
